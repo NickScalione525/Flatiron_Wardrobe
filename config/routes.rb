35bf1a42db3bdb6c218594cd53c0c1463f47cd0d
nick <nicholas_scalione@yahoo.com>
2021-04-03 18:18:43 -0400

    updated my application layout and nested appropriate routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+
   resources :styles
-  resources :outfits
-  resources :users
+  resources :outfits do 
+    resources :styles
+  end
+  resources :users do
+    resources :outfits
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
