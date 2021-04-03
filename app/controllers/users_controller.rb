d0964de87ac4ae42dd53d75939ce75be18854c89
nick <nicholas_scalione@yahoo.com>
2021-04-03 18:03:23 -0400

    Built out my users controller for initial functionality, added bcyrpt gem and has secure method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,33 @@
 class UsersController < ApplicationController
+    
+
+    def new
+        @user = User.new
+    end
+
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user_id
+            redirect_to @user
+        else
+            render ':users/new'
+        end
+    end
+
+    def show
+        @user = User.find_by_id(params[:id])
+        redirect_to '/' if !@user
+
+
+    end
+
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
     has_many :outfits
     has_many :styles, through: :outfits
+
+    has_secure_password
+
+
+    
 end
